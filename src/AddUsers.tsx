2bf6e05c6080663071f748352349337abb4bfe17
Herb Caudill <herb@devresults.com>
2019-08-27 12:03:39 +0200

    use generator, show progress

--- src/AddUsers.tsx
+++ src/AddUsers.tsx
@@ -7,13 +7,12 @@ const addUsersWorker = () => new Worker('./addUsers.worker.ts')
 const AddUsers: React.FC<{ count: number }> = ({ count }) => {
   const { result, error } = useWorker(addUsersWorker, count)
 
-  return error ? (
-    <div>Error: {JSON.stringify(error)}</div>
-  ) : result ? (
-    <div>Added {result} users</div>
-  ) : (
+  return (
+    error ? <div>Error: {JSON.stringify(error)}</div> :
+      result ? <div>Added {result} users</div> :
         <span />
-      )
+  )
+
 }
 
 export default AddUsers
--- src/addUsers.worker.ts
+++ src/addUsers.worker.ts
@@ -1,12 +1,13 @@
 import { exposeWorker } from 'react-hooks-worker'
 import * as A from 'automerge'
 
-const addUsers = (n: number) => {
+async function* addUsers(n: number) {
   let doc = A.from({ users: [] })
   for (let i = 0; i < n; i++) {
+    yield i
     doc = A.change(doc, d => d.users.push({ id: i }))
   }
-  return n
+  yield n
 }
 
 exposeWorker(addUsers)
