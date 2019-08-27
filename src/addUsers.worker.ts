4418fb667dfcacfff38dc95960d9072017595d98
Herb Caudill <herb@devresults.com>
2019-08-27 11:56:39 +0200

    worker to typescript

--- src/addUsers.worker.ts
+++ src/addUsers.worker.ts
@@ -1,14 +1,12 @@
 import { exposeWorker } from 'react-hooks-worker'
 import * as A from 'automerge'
 
-const addUsers = n => {
+const addUsers = (n: number) => {
   let doc = A.from({ users: [] })
   for (let i = 0; i < n; i++) {
     doc = A.change(doc, d => d.users.push({ id: i }))
   }
   return n
 }
-
-const foo = (action, payload) => {}
 
 exposeWorker(addUsers)
